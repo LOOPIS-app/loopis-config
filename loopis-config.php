f2f2599a2363b4b967554f4f2ebd9f6846c4e5a2
Johan <linger.konsult@gmail.com>
2025-09-04 13:21:48 +0200

    Måste flytta upp denna fil för att det skall fungera direkt mot Github och i wordpress - så att pluginen syns så fort man checkat ut koden till under plugins

---
+++ loopis-config.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: LOOPIS Config
+Version: 0.0
+Author: develoopers
+Author URI: https://loopis.org
+Description: Plugin for configuring a clean WP installation for LOOPIS.app
+*/
+
+// Prevent direct access
+if (!defined('ABSPATH')) { 
+    exit; 
+}
+
