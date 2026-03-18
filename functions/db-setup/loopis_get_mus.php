7c87e1559f943835ced3b14b7836f5dde3154a99
HubertHilborn <hubert.hilborn@hotmail.com>
2026-03-18 11:56:08 +0100

    Base-MU and tbf prio

---
+++ functions/db-setup/loopis_get_mus.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Function to install LOOPIS MU-plugins.
+ *
+ * This function is called by main function 'loopis_db_setup'.
+ * 
+ * @package LOOPIS_Config
+ * @subpackage Plugins
+ */
+
+ /**
+ * Installs a MU plugin from a ZIP URL into wp-content/mu-plugins
+ *
+ * @return void
+ */
+function loopis_mu_install() {
+    loopis_elog_function_start('loopis_mu_install');
+
+    // Get install data from POST
+    $data = $_POST['data'] ?? [];
+    $slug = sanitize_text_field($data['slug'] ?? '');
+    $zip_url = sanitize_text_field($data['zip_url'] ?? '');
+
+    if (empty($slug) || empty($zip_url)) {
+        loopis_elog_first_level("Missing slug or zip URL");
+        return;
+    }
+
+    $mu_dir = WPMU_PLUGIN_DIR;
+    if (!file_exists($mu_dir)) {
+        mkdir($mu_dir, 0755, true);
+        loopis_elog_first_level("Created mu-plugins directory!");
+    }
+
+    // Download ZIP to a temporary file
+    $tmp_file = download_url($zip_url);
+
+    if (is_wp_error($tmp_file)) {
+        loopis_elog_first_level("Failed to download ZIP: " . $tmp_file->get_error_message());
+        return;
+    }
+    loopis_elog_first_level("Downloaded MU-ZIP!");
+    // Extract ZIP
+    $zip = new ZipArchive();
+    if ($zip->open($tmp_file) === true) {
+        $zip->extractTo($mu_dir);
+        $zip->close();
+        loopis_elog_first_level("Extracted $slug to MU plugins");
+    } else {
+        loopis_elog_first_level("Failed to open ZIP for $slug");
+        @unlink($tmp_file);
+        return;
+    }
+
+    @unlink($tmp_file);
+
+    loopis_elog_first_level("MU plugins installed successfully");
+
+    loopis_elog_function_end_success('loopis_mu_install');
+}
--- loopis-config.php
+++ loopis-config.php
@@ -17,15 +17,6 @@ Author URI: https://loopis.org
  * (at your option) any later version.
  */
 
-/*
- * Copyright (C) 2026 LOOPIS
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- */
-
 // Prevent direct access
 if (!defined('ABSPATH')) { 
     exit; 
